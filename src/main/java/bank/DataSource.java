f7041e67e7ca73adfe2f77856213b825f9765c82
Pvan <pawankumarp2000@outlook.com>
2023-06-15 21:06:38 +0000

    model updated

---
+++ src/main/java/bank/DataSource.java
@@ -0,0 +1,50 @@
+package bank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DataSource {
+
+  public static Connection connect(){
+    String db_file = "jdbc:sqlite:resources/bank.db";
+    Connection connection = null;
+    try {
+      connection = DriverManager.getConnection(db_file);
+      System.out.println("We're connected");
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return connection;
+  }
+
+  public static Customer getCustomer(String username){
+    String sql = "select * from customers where username = ?";
+    Customer customer = null;
+
+    try(Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)){
+        statement.setString(1, username);
+        try(ResultSet resultSet = statement.executeQuery()) {
+          customer = new Customer(
+            resultSet.getInt("id"),
+            resultSet.getString("name"), 
+            resultSet.getString("username"), 
+            resultSet.getString("password"),
+            resultSet.getInt("account_Id"));
+        }  
+    }catch (SQLException e) {
+      // TODO: handle exception
+      e.printStackTrace();
+    }
+    return customer;
+  }
+
+  public static void main(String[] args) {
+    Customer customer = getCustomer("clillea8@nasa.gov");
+    System.out.println(customer.getName());
+  }
+  
+}
